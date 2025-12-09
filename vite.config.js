7f8a168e50eec5b12801c78a7c573fa574adf020
Melomegit100706 <ulrichmelome@gmail.com>
2025-12-09 09:20:29 +0100

    Commit fait

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
-import vue from '@vitejs/plugin-vue';
 import react from '@vitejs/plugin-react';
 
 
@@ -11,6 +10,5 @@ export default defineConfig({
       refresh: true,
     }),
     react(),
-    vue(),
   ],
 })
