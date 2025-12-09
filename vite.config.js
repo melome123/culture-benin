0e7e957dac379ccbe6a83b5d6f4ef16ca92261bd
Melomegit100706 <ulrichmelome@gmail.com>
2025-12-09 09:17:26 +0100

    Commit fait

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue';
+import react from '@vitejs/plugin-react';
 
 
 export default defineConfig({
@@ -8,5 +10,7 @@ export default defineConfig({
       input: ['resources/css/app.css','resources/js/app.jsx', 'resources/js/app.js'],
       refresh: true,
     }),
+    react(),
+    vue(),
   ],
 })
